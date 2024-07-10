69dc30bd06ee87a26588b7144ea161b15eec1f49
Lucas Nussbaum <lucas.obione@gmail.com>
2024-07-10 16:03:30 -0400

    added animation button to intro

--- 00-intro/intro.js
+++ 00-intro/intro.js
@@ -9,9 +9,27 @@ const fontSizeField = document.getElementById("font-size-field");
 const clickButton = document.getElementById("click-button");
 const text = document.getElementById("text");
 const body = document.body;
+const checkbox = document.getElementById("checkbox")
+const animationButton = document.getElementById("animation-button")
+const robot= document.getElementById("robot")
 function buttonPress(){
     text.innerHTML = "you pressed the button";
 
     body.style.backgroundColor = colorField.value;
     text.style.fontSize = `${fontSizeField.value}px`
+}
+
+function checkboxChange(){
+    let checked = checkbox.checked;
+    console.log(`The checkbox has this state: ${checked}`)
+
+    if(checked){
+        body.style.outline = "solid";
+    } else {
+        body.style.outline = "none";
+    }
+}
+
+function animationPress(){
+    robot.style.visibility ='visible'
 }
---
+++ home.js
@@ -0,0 +1,7 @@
+const invertButton = document.getElementById("invert-color")
+const body = document.body;
+
+function buttonPressed(){
+    body.style.backgroundColor = "green";
+
+}
