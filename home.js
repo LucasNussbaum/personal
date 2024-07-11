ed4df07574c2c5afe16c3b3b289977bbbafb5601
Lucas Nussbaum <lucas.obione@gmail.com>
2024-07-11 09:19:53 -0400

    added interactive to website and started working 01-numgame

--- home.js
+++ home.js
@@ -1,7 +1,21 @@
 const invertButton = document.getElementById("invert-color")
 const body = document.body;
+const colorField = document.getElementById("color-field");
+const fontSizeField = document.getElementById("font-size-field");
 
-function buttonPressed(){
-    body.style.backgroundColor = "green";
 
+function toggleInvert() {
+    document.body.classList.toggle('invert');
 }
+
+function buttonPress(){
+
+    body.style.backgroundColor = colorField.value;
+
+    const paragraphArray = document.getElementsByTagName("p");
+    for (let paragraph of paragraphArray) {
+        console.log('gt')
+        paragraph.style.fontSize = `${fontSizeField.value}px`
+    }
+}
+
